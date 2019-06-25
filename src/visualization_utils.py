ba9fee65651c721e1b39aea29300cb56128a5e68
Josh Joseph <josh.m.joseph@gmail.com>
2019-06-25 08:52:26 -0400

    moved viz from brain_utils into its own util

--- src/visualization_utils.py
+++ src/visualization_utils.py
@@ -6,7 +6,6 @@ import seaborn as sns
 DEFAULT_FIG_SIZE = (7, 2)
 
 
-
 def visualize_weights(brain_state, figsize=DEFAULT_FIG_SIZE):
 
     f, axs = plt.subplots(1, len(brain_state['layer_weights']), figsize=figsize, constrained_layout=True)
@@ -28,21 +27,3 @@ def visualize_activations(brain_state, figsize=DEFAULT_FIG_SIZE):
         axs[i].set_title('Activations {}'.format(i))
 
     f.suptitle("network activations at time t", weight='bold', size=16)
-
-
-
-
-if __name__ == "__main__":
-    from src.training import load_pretrained_agent
-
-    agent = load_pretrained_agent()
-
-    from src import world
-
-    episode_history = world.run_episode(agent, visualize_behavior=False)
-
-    episode_index = 50
-    brain_state = get_brain_state(agent, episode_history['observation'][episode_index])
-    visualize_weights(brain_state)
-    visualize_activations(brain_state)
-    print(1)
